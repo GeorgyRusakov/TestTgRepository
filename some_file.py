a27cd047b1abca644ac835863e6e20f309c957d9
Georgy R <rusakov.georgy@gmail.com>
2025-06-23 18:59:41 +0300

    Добавление в файл

--- some_file.py
+++ some_file.py
@@ -1,3 +1,13 @@
-print('Hello, GIT')
+from pydantic import field_validator, BaseModel
+from datetime import date
 
-print('some changes code')
+
+my_date = date(year=2003, month=4, day=19)
+
+today = date.today()
+
+age = today.year - my_date.year - ((today.month, today.day) < (my_date.month, my_date.day))
+
+print(age)
+
+
